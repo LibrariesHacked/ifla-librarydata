53a751b766406af82295467086b64f6239b5cc4b
Dave <DaveBathnes@users.noreply.github.com>
2018-11-21 20:19:47 +0000

    Finishing initial script

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Looking at the data for the IFLA Map.
 
+# Licence
+
+All code in this repository licensed as open source with the MIT Licence.
+
+The [IFLA library data](https://librarymap.ifla.org/copyright) is released under Creative Commons Attribution 4.0 International. 
--- create_csv.py
+++ create_csv.py
@@ -2,19 +2,23 @@
 import csv
 import json
 
+# In future directly use https://librarymap.ifla.org/api/map
+
 COUNTRY_DATA = 'countries_pop.txt'
 LOOKUP_DATA = 'map.json'
+OUTPUT_DATA = 'ifla_data.csv'
+OUTPUT_TEXT = 'ifla_text.json'
 
 def read_lookup_data():
     """Return 4 data sets for countries, metrics, languages, and contributors"""
 
-    lookups = { 'countries': [], 'metrics': [], 'languages': [], 'contributors': [] }
+    lookups = { 'countries': [], 'metrics': [], 'languages': [], 'contributors': [], 'values': [], 'libraryTypes': [] }
     with open(LOOKUP_DATA, encoding='utf-8') as data_file:
         data = json.loads(data_file.read())
         for idx, item in enumerate(lookups.items()): # For each lookup type
             for row in data[item[0]]:
                 lookups[item[0]].append(row)
-    
+
     return lookups
 
 def run():
@@ -23,24 +27,48 @@ def run():
     lookups = read_lookup_data()
 
     ## Our single CSV will essentially be every country, with each column being associated data taken from other lookups
-    countries = []
+    countries_data = []
+    set_headers = ['name', 'continent', 'sub_region', 'intermediate_region', 'updated_at']
+    dynamic_headers = []
 
     for country in lookups['countries']:
 
-        contributors = []
-        for contributor in lookups['contributors']:
-            if contributor['country_id'] == country['id']:
-                contributors.append(contributor);
+        # Standard values
+        country_data = {}
+        for header in set_headers:
+            country_data[header] = country[header]
 
-        print(country['name'])
-        print(contributors)
+        # Get all the metric values for the country
+        country_value_data = {}
+        for value in lookups['values']:
+            if value['country_id'] == country['id']:
+                if value['library_type_id'] not in country_value_data:
+                    country_value_data[value['library_type_id']] = {}
+                country_value_data[value['library_type_id']][value['metric_id']] = value['val']
 
-        # Country values to take: name, continent, sub_region, intermediate_region
-        country_data = {
-            'name': country['name'],
-            'continent': country['continent'],
-            'sub_region': country['sub_region'],
-            'intermediate_region': country['intermediate_region']
-        }
+        # Assign the metric values
+        for metric in lookups['metrics']:
+
+            for type in lookups['libraryTypes']:
+                if (type['name'] + '_' + metric['shortname']) not in dynamic_headers:
+                    dynamic_headers.append(type['name'] + '_' + metric['shortname'])
+                if type['id'] in country_value_data and metric['id'] in country_value_data[type['id']]:
+                    country_data[type['name'] + '_' + metric['shortname']] = country_value_data[type['id']][metric['id']]
+
+        countries_data.append(country_data)
+
+    headers = set_headers + dynamic_headers
+
+    with open(OUTPUT_DATA, 'w', newline='') as outputfile:
+        csvwriter = csv.writer(outputfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(headers)
+        for country in countries_data:
+            country_row = []
+            for header in headers:
+                if header in country:
+                    country_row.append(country[header])
+                else:
+                    country_row.append('')
+            csvwriter.writerow(country_row)
 
 run()
